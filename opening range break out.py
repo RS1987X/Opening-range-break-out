9eeed86417cba79e26d176a7326cb880e7e93db4
RS1987X <richard.kj.steffen@gmail.com>
2021-12-13 17:32:09 +0100

    new datasets

--- opening range break out.py
+++ opening range break out.py
@@ -15,7 +15,7 @@ from datetime import date
 from datetime import datetime
 from dateutil import parser
 
-evo_data = pd.read_csv('OMXSTO_DLY_EVO, 15.csv')
+evo_data = pd.read_csv('OMXSTO_DLY_SINCH, 15.csv')
 evo_data = evo_data['time;open;high;low;close;VWAP;Upper Band;Lower Band;Volume;Volume MA'].str.split(";",expand=True)
 evo_data = evo_data.rename(columns={0:"DateTime", 1:"Open", 2:"High", 3:"Low", 4:"Close", 5:"VWAP", 6:"Upper Band", 7:"Lower Band", 8:"Volume", 9:"Volume MA"})
 evo_data = evo_data[["DateTime","Open","High", "Low", "Close","Volume"]]
@@ -31,14 +31,31 @@ evo_data.insert(2,"TimePart", only_time_part)
 
 
 
-#evo_data = evo_data.set_index("DateTime")
 
-#datetime.strptime(evo_data.index[0],'%Y-%m-%dT%H:%M:%S').time()
+#Closing prices for the trading session, full and half session
+full_day_dates = evo_data[evo_data["TimePart"] == "17:15:00"]["DatePart"].to_frame()
+all_dates =  evo_data[evo_data["TimePart"] == "09:00:00"]["DatePart"].to_frame()
+idx = np.where(all_dates.merge(full_day_dates,how="left",indicator=True)["_merge"] == "left_only")
+half_day_dates = all_dates.iloc[idx]["DatePart"].to_frame()
+half_days_data = evo_data[evo_data["DatePart"].isin(half_day_dates["DatePart"])]
+half_days_data["TimePart"] == "12:45:00"
 
+
+#DAY HIGH and DAY LOWS
 dh = evo_data.groupby('DatePart')["High"].max().to_frame()
 dl = evo_data.groupby('DatePart')["Low"].min().to_frame()
-#dh = dh.to_frame()
 
+#EXIT PRICES for both half and full sessions
+exit_price_half_day = half_days_data[half_days_data["TimePart"] == "12:45:00"]["Close"]
+exit_price_full_days = evo_data[evo_data["TimePart"] == "17:15:00"]["Close"]
+exit_price = exit_price_full_days.append(exit_price_half_day)
+
+exit_price = exit_price.sort_index()
+exit_price = exit_price.to_frame().astype(float)
+exit_price.insert(1,"DatePart",only_date_part)
+exit_price = exit_price.set_index("DatePart")
+
+#OPENING RANGE
 opening_rng_high = evo_data[evo_data["TimePart"] == "09:00:00"]["High"].to_frame()
 #opening_rng_high = opening_rng_high.to_frame()
 opening_rng_high.insert(1,"DatePart",only_date_part)
@@ -50,43 +67,29 @@ opening_rng_low = evo_data[evo_data["TimePart"] == "09:00:00"]["Low"].to_frame()
 opening_rng_low.insert(1,"DatePart",only_date_part)
 opening_rng_low = opening_rng_low.set_index("DatePart")
 
-
+#OPEN BAR VOLUME
 opening_rng_volume = evo_data[evo_data["TimePart"] == "09:00:00"]["Volume"].to_frame()
 opening_rng_volume.insert(1,"DatePart",only_date_part)
 opening_rng_volume = opening_rng_volume.set_index("DatePart")
 
+#OPENING GAP 
+open_price = evo_data[evo_data["TimePart"] == "09:00:00"]["Open"].to_frame().astype(float)
+open_price.insert(1,"DatePart",only_date_part)
+open_price = open_price.set_index("DatePart")
+opening_gap = open_price["Open"]/exit_price["Close"].shift(1)-1
+
 
 #long position logic
 dh_above_opening_high = (dh > opening_rng_high)
-low_opening_rng_volume = (opening_rng_volume["Volume"].astype(int) < (1*200000)).to_frame()
+low_opening_rng_volume = (opening_rng_volume["Volume"].astype(int) < (1*150000)).to_frame()
 
 #short position logic
 #dl_below_opening_low = (dh > opening_rng_high)
 #low_opening_rng_volume = (opening_rng_volume["Volume"].astype(int) > 300000).to_frame()
 
 
-pos_ind = dh_above_opening_high["High"] & low_opening_rng_volume["Volume"]
+pos_ind = (dh_above_opening_high["High"])  & (low_opening_rng_volume["Volume"]) #& (abs(opening_gap) < 0.1)
 entry_price = opening_rng_high[pos_ind].astype(float)
-
-
-full_day_dates = evo_data[evo_data["TimePart"] == "17:15:00"]["DatePart"].to_frame()
-all_dates =  evo_data[evo_data["TimePart"] == "09:00:00"]["DatePart"].to_frame()
-idx = np.where(all_dates.merge(full_day_dates,how="left",indicator=True)["_merge"] == "left_only")
-half_day_dates = all_dates.iloc[idx]["DatePart"].to_frame()
-
-
-half_days_data = evo_data[evo_data["DatePart"].isin(half_day_dates["DatePart"])]
-half_days_data["TimePart"] == "12:45:00"
-
-
-exit_price_half_day = half_days_data[half_days_data["TimePart"] == "12:45:00"]["Close"]
-exit_price_full_days = evo_data[evo_data["TimePart"] == "17:15:00"]["Close"]
-exit_price = exit_price_full_days.append(exit_price_half_day)
-
-exit_price = exit_price.sort_index()
-exit_price = exit_price.to_frame()
-exit_price.insert(1,"DatePart",only_date_part)
-exit_price = exit_price.set_index("DatePart")
 
 
 #entry_price_no_nan = entry_price[~entry_price["High"].isnull()].astype(float)
@@ -95,6 +98,7 @@ exit_price = exit_price.set_index("DatePart")
 
 #is stop loss hit?
 #exit_price[exit_price["Close"].astype(float) < opening_rng_low["Low"].astype(float)] =  opening_rng_low
+#TAKE OUT closing prices where we had an trade
 exit_price = exit_price[pos_ind].astype(float)
 
 
@@ -110,8 +114,8 @@ print("volatility " + str(strat_returns.std()))
 
 kelly_f = strat_returns.mean()/(strat_returns.std()**2)
 print("kelly f " + str(kelly_f))
-
-
+percent_profitable = (strat_returns > 0).sum()/len(strat_returns)
+print("Percent profitable " + str(percent_profitable))
 ############################################
 ##stats for basic strategy
 ###########################################

f944670d87ec2fe5fe692e921eccec30bfed2b92
RS1987X <richard.kj.steffen@gmail.com>
2021-12-28 11:49:53 +0100

    data

--- opening range break out.py
+++ opening range break out.py
@@ -16,7 +16,7 @@ from datetime import datetime
 from dateutil import parser
 from statsmodels.graphics.tsaplots import plot_acf
 
-evo_data = pd.read_csv('OMXSTO_DLY_EQT, 15.csv')
+evo_data = pd.read_csv('OMXSTO_DLY_SF, 15.csv')
 evo_data = evo_data['time;open;high;low;close;VWAP;Upper Band;Lower Band;Volume;Volume MA'].str.split(";",expand=True)
 evo_data = evo_data.rename(columns={0:"DateTime", 1:"Open", 2:"High", 3:"Low", 4:"Close", 5:"VWAP", 6:"Upper Band", 7:"Lower Band", 8:"Volume", 9:"Volume MA"})
 evo_data = evo_data[["DateTime","Open","High", "Low", "Close","Volume"]]
